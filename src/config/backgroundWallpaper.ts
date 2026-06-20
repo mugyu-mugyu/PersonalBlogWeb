ed93a96783767f1c58fdfdf73425c79eead67606
mugyu-mugyu <whutwsf@whut.edu.cn>
2026-06-21 03:49:46 +0800

    修改配置

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -45,8 +45,6 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		desktop: [
 			"assets/images/DesktopWallpaper/d1.avif",
 			"assets/images/DesktopWallpaper/d2.avif",
-			"assets/images/DesktopWallpaper/d3.avif",
-			"assets/images/DesktopWallpaper/d4.avif",
 		],
 		// 移动背景图片（支持单张或多张随机）
 		// mobile: "assets/images/MobileWallpaper/m1.avif",
