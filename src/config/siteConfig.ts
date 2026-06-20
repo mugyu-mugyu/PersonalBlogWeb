f7117156d1400a4b7d5d98319b30656aadefae2b
mugyu-mugyu <whutwsf@whut.edu.cn>
2026-06-21 01:50:11 +0800

    config:修改配置

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,10 +6,10 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "橙花小猫",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "个人技术博客",
 
 	// 站点 URL
 	site_url: "https://firefly.cuteleaf.cn",
