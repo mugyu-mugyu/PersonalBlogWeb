1aff3e803e3f2aaf7bcabb4b7be39a9b4c9cdb9f
mugyu-mugyu <whutwsf@whut.edu.cn>
2026-06-21 17:15:59 +0800

    修改配置

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -144,9 +144,9 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// 横幅图片轮播配置，仅在当配置多张图片时生效
 		carousel: {
 			// 是否启用横幅图片轮播；关闭时保持每次刷新随机显示一张
-			enable: false,
+			enable: true,
 			// 轮播切换间隔（毫秒）
-			interval: 5000,
+			interval: 10000,
 			// 是否允许用户通过控制面板切换横幅轮播
 			switchable: false,
 		},
